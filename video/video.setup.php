e392cd26984549d4775d615c6ca9ec8b0ed5b98f
Vladimir Sibirov <trustmaster@kodigy.com>
2013-05-18 23:11:32 +0400

    Small update for setup file

--- video/video.setup.php
+++ video/video.setup.php
@@ -5,7 +5,7 @@ Code=video
 Name=Video
 Description=Video Catalog, displaying videos from public video hostings
 Version=1.0.0
-Date=2013-04-27
+Date=2013-05-18
 Author=Vladimir Sirirov
 Copyright=(c) Vladimir Sirirov, 2013
 Notes=
@@ -17,7 +17,7 @@ Lock_members=2345
 
 [BEGIN_COT_EXT_CONFIG]
 perindex=01:string::3:Videos displayed on index page
-perpage=02:string::5:Videos displayed per page in catalogue
+perpage=02:string::10:Videos displayed per page in catalogue
 index_width=03:string::308:Player width on index page
 index_height=04:string::185:Player height on index page
 main_width=05:string::853:Player width in the catalog
